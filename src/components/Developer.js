791d97155eb0540962999c31f3e0c95c53db40a4
Basruhe <sb.ruhe@gmail.com>
2020-01-29 12:00:55 +0100

    starting on developer detail page

---
+++ src/components/Developer.js
@@ -0,0 +1,11 @@
+// Add a Developer component which displays a single developer (say, their name and email address).
+//Loop over the developers and use this new component to display each one of them. console.log(this.props.dev) first to check what you are getting as props from the store.
+
+// TODO:
+// import developerlist
+// console log list to see data. (console.log(this.props.dev))
+// map the developerlist over a <ul><li>, render.
+// import Developer in app.js
+
+import React from "react";
+import DevelopersList from "./DevelopersList";
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,16 +1,24 @@
 // src/store/reducer.js
+// This is the root reducer. More subreducers will be added here
 import { combineReducers } from "redux";
+
+import developers from "./developers/reducer";
+
 export default combineReducers({
-  havingFun: havingFunReducer
-  // we can add more "slice" subreducers here later on...
+  developers
 });
-function havingFunReducer(state = "yes", action) {
-  switch (action.type) {
-    case "SET_HAVING_FUN": {
-      return action.payload;
-    }
-    default: {
-      return state;
-    }
-  }
-}
+
+// export default combineReducers({
+//   havingFun: havingFunReducer
+//   // we can add more "slice" subreducers here later on...
+// });
+// function havingFunReducer(state = "yes", action) {
+//   switch (action.type) {
+//     case "SET_HAVING_FUN": {
+//       return action.payload;
+//     }
+//     default: {
+//       return state;
+//     }
+//   }
+// }
